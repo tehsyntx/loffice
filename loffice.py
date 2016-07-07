617c3bf4c6cbf8e3bddd7c98ce4a1183cf046815
mgeeky <mgeeky@users.noreply.github.com>
2016-07-07 22:20:14 +0200

    Refactored code a bit to move all of the message dispaying to the Logging module (will be helpful in automated analysis tools that will like to redirect logging to some file). Also introduced options parsing using optparse.

--- loffice.py
+++ loffice.py
@@ -14,9 +14,18 @@ Author: @tehsyntx
 from winappdbg import Debug, EventHandler
 import sys
 import os
+import optparse
+import logging
+
+logging.basicConfig(format='%(levelname)s%(message)s')
+logging.addLevelName( logging.INFO, '')
+logging.addLevelName( logging.DEBUG, '[%s] ' % logging.getLevelName(logging.DEBUG))
+logging.addLevelName( logging.ERROR, '[%s] ' % logging.getLevelName(logging.ERROR))
+logger = logging.getLogger()
 
 # Root path to Microsoft Office
-OFFICE_PATH = 'C:\\Program Files\\Microsoft Office\\Office15\\'
+DEFAULT_OFFICE_PATH = 'C:\\Program Files\\Microsoft Office\\Office15'
+
 
 def cb_crackurl(event):
 	
@@ -25,10 +34,10 @@ def cb_crackurl(event):
 
 	lpszUrl = thread.read_stack_dwords(2)[1]
 
-	print 'FOUND URL\n\t%s\n' % proc.peek_string(lpszUrl, fUnicode=True)
+	logger.info('FOUND URL:\n\t%s\n' % proc.peek_string(lpszUrl, fUnicode=True))
 
 	if exit_on == 'url':
-		print 'Exiting on first URL, bye!'
+		logger.info('Exiting on first URL, bye!')
 		sys.exit()
 
 		
@@ -40,7 +49,7 @@ def cb_createfilew(event):
 	lpFileName, dwDesiredAccess = thread.read_stack_dwords(3)[1:]
 
 	if dwDesiredAccess == 0x80000100:
-		print 'OPEN FILE HANDLE\n\t%s\n' % (proc.peek_string(lpFileName, fUnicode=True))
+		logger.info('OPEN FILE HANDLE\n\t%s\n' % (proc.peek_string(lpFileName, fUnicode=True)))
 
 		
 def cb_createprocessw(event):
@@ -52,14 +61,14 @@ def cb_createprocessw(event):
 	application = proc.peek_string(lpApplicationName, fUnicode=True)
 	cmdline = proc.peek_string(lpCommandLine, fUnicode=True)
 
-	print 'CREATE PROCESS\n\tApp: "%s"\n\tCmd-line: "%s"\n' % (application, cmdline)		
+	logger.info('CREATE PROCESS\n\tApp: "%s"\n\tCmd-line: "%s"\n' % (application, cmdline))
 	
 	if exit_on == 'url' and 'splwow64' not in application:
-		print 'Process created before URL was found, exiting for safety'
+		logger.info('Process created before URL was found, exiting for safety')
 		sys.exit()
 		
 	if exit_on == 'proc' and 'splwow64' not in application:
-		print 'Exiting on process creation, bye!'
+		logger.info('Exiting on process creation, bye!')
 		sys.exit()
 
 def cb_stubclient20(event):
@@ -67,15 +76,15 @@ def cb_stubclient20(event):
 	proc = event.get_process()
 	thread  = event.get_thread()
 
-	print 'DETECTED WMI QUERY'
+	logger.info('DETECTED WMI QUERY')
 
 	strQueryLanguage, strQuery = thread.read_stack_dwords(4)[2:]
 
 	language = proc.peek_string(strQueryLanguage, fUnicode=True)
 	query = proc.peek_string(strQuery, fUnicode=True)
 
-	print '\tLanguage: %s' % language
-	print '\tQuery: %s' % query
+	logger.info('\tLanguage: %s' % language)
+	logger.info('\tQuery: %s' % query)
 
 	if 'win32_product' in query.lower() or 'win32_process' in query.lower():
 
@@ -95,7 +104,7 @@ def cb_stubclient20(event):
 
 		patched_query = proc.peek_string(strQuery, fUnicode=True)
 
-		print '\tPatched with: %s' % patched_query
+		logger.info('\tPatched with: %s' % patched_query)
 
 
 class EventHandler(EventHandler):
@@ -110,91 +119,120 @@ class EventHandler(EventHandler):
 			try:
 				event.debug.break_at(pid, address, cb_createprocessw)
 			except:
-				print '[?] Could not break at CreateProcessW'
+				logger.error('Could not break at: CreateProcessW')
 
 			address = module.resolve("CreateFileW")
 			try:
 				event.debug.break_at(pid, address, cb_createfilew)
 			except:
-				print '[?] Could not break at CreateFileW'
+				logger.error('Could not break at: CreateFileW')
 			
 		if module.match_name("wininet.dll"):
 			address = module.resolve("InternetCrackUrlW")
 			try:
 				event.debug.break_at(pid, address, cb_crackurl)
 			except:
-				print '[?] Could not break at InternetCrackUrlW'
+				logger.error('Could not break at: InternetCrackUrlW')
 
 		if module.match_name("winhttp.dll"):
 			address = module.resolve("WinHttpCrackUrl")
 			try:
 				event.debug.break_at(pid, address, cb_crackurl)
 			except:
-				print '[?] Could not break at WinHttpCrackUrl'
+				logger.error('Could not break at: WinHttpCrackUrl')
 
 		if module.match_name("ole32.dll"):
 			address = module.resolve("ObjectStublessClient20")
 			try:
 				event.debug.break_at(pid, address, cb_stubclient20)
 			except:
-				print '[?] Could not break at ObjectStublessClient20'
+				logger.error('Could not break at: ObjectStublessClient20')
 
-		
-def usage():
 
-	print ''
-	print 'Lazy Office Analyzer - Analyze documents with WinDbg'
-	print ''
-	print 'loffice.py [type] [exit-on] [filename]'
-	print 'Type:'
-	print '\tword   - Word document'
-	print '\texcel  - Excel spreadsheet'
-	print '\tpower  - Powerpoint document'
-	print '\tscript - VBscript & Javascript'
-	print 'Exit-on:'
-	print '\turl  - After first URL extraction (no remote fetching)'
-	print '\tproc - Before process creation (allow remote fetching)'
-	print '\tnone - Allow uniterupted execution (dangerous)'
+def options():
+
+	valid_types = ['word', 'excel', 'power', 'script']
+	valid_exit_ons = ['url', 'proc', 'none']
+
+	usage = '''
+	%prog [options] <type> <exit-on> <filename>
 	
-	sys.exit()
-	
+Type:
+	word   - Word document
+	excel  - Excel spreadsheet
+	power  - Powerpoint document
+	script - VBscript & Javascript
+
+Exit-on:
+	url  - After first URL extraction (no remote fetching)
+	proc - Before process creation (allow remote fetching)
+	none - Allow uniterupted execution (dangerous)
+'''
+	parser = optparse.OptionParser(usage=usage)
+	parser.add_option('-v', '--verbose', dest='verbose', help='Verbose mode.', action='store_true')
+	parser.add_option('-p', '--path', dest='path', help='Path to the Microsoft Office suite.', default=DEFAULT_OFFICE_PATH)
+
+	opts, args = parser.parse_args()
+
+	if not os.path.exists(opts.path):
+		logger.error('Specified Office path does not exists: "%s"' % opts.path)
+		sys.exit(1)
+
+	if args[0] not in valid_types:
+		logger.error('Specified <type> is not recognized: "%s".' % args[0])
+		sys.exit(1)
+
+	if args[1] not in valid_exit_ons:
+		logger.error('Specified <exit-on> is not recognized: "%s".' % args[1])
+		sys.exit(1)
+
+	if not os.path.isfile(args[2]):
+		logger.error('Specified file to analyse does not exists: "%s"' % args[2])
+		sys.exit(1)
+
+	if opts.verbose:
+		logger.setLevel(logging.DEBUG)
+	else:
+		logger.setLevel(logging.INFO)
+
+	return (opts, args)
+
+
 if __name__ == "__main__":
 
-	if len(sys.argv) < 4:
-		usage()
+	(opts, args) = options()
 
-	args = []
+	logger.info('\n\tLazy Office Analyzer - Analyze documents with WinDbg\n')
 
-	if sys.argv[1] == 'script':
-		args.append('%s\\system32\\wscript.exe' % os.environ['WINDIR'])
-	elif sys.argv[1] == 'word':
-		args.append('%s\\WINWORD.EXE' % OFFICE_PATH)
-	elif sys.argv[1] == 'excel':
-		args.append('%s\\EXCEL.EXE' % OFFICE_PATH)
-	elif sys.argv[1] == 'power':
-		args.append('%s\\POWERPNT.EXE' % OFFICE_PATH)
+	office_invoke = []
+	if args[0] == 'script':
+		office_invoke.append('%s\\system32\\wscript.exe' % os.environ['WINDIR'])
+	elif args[0] == 'word':
+		office_invoke.append('%s\\WINWORD.EXE' % opts.path)
+	elif args[0] == 'excel':
+		office_invoke.append('%s\\EXCEL.EXE' % opts.path)
+	elif args[0] == 'power':
+		office_invoke.append('%s\\POWERPNT.EXE' % opts.path)
 	else:
-		print 'Unsupported type: %s' % sys.argv[1]
-		sys.exit()
+		print 'Unsupported type: %s' % args[0]
+		sys.exit(1)
 
-	if not os.path.isfile(args[0]):
-		print 'Host process path could not be found: %s' % args[0]
-		sys.exit()
+	logger.debug('Using office path:')
+	logger.debug('\t"%s"' % office_invoke[0])
 		
 	global exit_on
-	if sys.argv[2] == 'url' or sys.argv[2] == 'proc' or sys.argv[2] == 'none':
-		exit_on = sys.argv[2]
-	else:
-		print 'Unsupported exit-on: %s' % sys.argv[2]
-		sys.exit()
+	exit_on = args[1]
 		
-	args.append(sys.argv[3]) # Document to analyze
+	office_invoke.append(args[2]) # Document to analyze
+
+	logger.debug('Invocation command:')
+	logger.debug('\t"%s"' % ' '.join(office_invoke))
 
 	with Debug(EventHandler(), bKillOnExit = True) as debug:
-		debug.execv(args)
+		debug.execv(office_invoke)
 		try:
-			print 'Launching...\n'
+			logger.debug('Launching...')
 			debug.loop()
 		except KeyboardInterrupt:
-			print 'Exiting, bye!'
+			logger.info('Exiting, bye!')
 			pass
